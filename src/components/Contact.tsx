082172ff0e19472e44f20ca33c48f628f481536e
Nilkanth-Patadiya <nilkanthpatadiya1@gmail.com>
2023-12-01 19:50:47 +0530

    Update contact page CSS

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ import {
   ListItemAvatar,
   ListItemText,
   Typography,
-  ListItem,
+  Link,
 } from "@mui/material"
 import { CommonProps } from "../App.props"
 import LinkedInIcon from "@mui/icons-material/LinkedIn"
@@ -16,16 +16,31 @@ import { CustomAvatar } from "../styles/styledComponents"
 const Contact = ({ secHeight, navID }: CommonProps) => {
   return (
     <section id={navID}>
-      <Grid container sx={{ minHeight: secHeight, alignItems: "flex-start" }}>
-        <Grid item xs={12}>
-          <Typography variant="h3" textAlign={"center"} my={3}>
-            Contact
+      <Grid container sx={{ minHeight: secHeight, alignContent: "flex-start" }}>
+        <Grid item xs={12} py={3} textAlign={"center"}>
+          <Typography variant="h4">Let's Get In Touch</Typography>
+          <Typography mt={1}>
+            Have a <Link underline="none">Question</Link> on your mind??
+          </Typography>
+          <Typography>
+            Or want a copy of my <Link underline="none">résumé</Link>? Contact
+            me using below links.
           </Typography>
         </Grid>
         <Grid item container alignItems={"center"} justifyContent={"center"}>
-          <Grid item>
-            <List sx={{ bgcolor: "background.paper" }}>
-              <ListItemButton href={`mailto:${emailID}`}>
+          <Grid item sx={{ bgcolor: "background.paper" }}>
+            <List>
+              <ListItemButton
+                href={`mailto:${emailID}`}
+                sx={{
+                  border: 3,
+                  borderRadius: 2,
+                  borderColor: "transparent",
+                  "&:hover": {
+                    borderColor: "primary.main",
+                  },
+                }}
+              >
                 <ListItemAvatar>
                   <CustomAvatar>
                     <EmailIcon fontSize="large" />
@@ -40,6 +55,14 @@ const Contact = ({ secHeight, navID }: CommonProps) => {
                 href={linkedInProfile}
                 target="_blank"
                 rel="noopener"
+                sx={{
+                  border: 3,
+                  borderRadius: 2,
+                  borderColor: "transparent",
+                  "&:hover": {
+                    borderColor: "primary.main",
+                  },
+                }}
               >
                 <ListItemAvatar>
                   <CustomAvatar>
@@ -51,7 +74,17 @@ const Contact = ({ secHeight, navID }: CommonProps) => {
                   primary="LinkedIn Profile"
                 />
               </ListItemButton>
-              <ListItem>
+              <ListItemButton
+                sx={{
+                  border: 3,
+                  borderRadius: 2,
+                  borderColor: "transparent",
+                  "&:hover": {
+                    borderColor: "primary.main",
+                    cursor: "initial",
+                  },
+                }}
+              >
                 <ListItemAvatar>
                   <CustomAvatar>
                     <LocationOnIcon fontSize="large" />
@@ -61,7 +94,7 @@ const Contact = ({ secHeight, navID }: CommonProps) => {
                   primaryTypographyProps={{ pl: 2, variant: "h6" }}
                   primary={myAddress}
                 />
-              </ListItem>
+              </ListItemButton>
             </List>
           </Grid>
         </Grid>
